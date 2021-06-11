d9fa602ee08ccef4e5b0ebc4d99a000d80387a4b
Sh4rpGr1nder <shggreen@hotmail.com>
2021-06-11 14:09:58 +0900

    mamacs

--- init.el
+++ init.el
@@ -30,8 +30,8 @@
 
 (setq org-log-done t)
 
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
-(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
+;;(projectile-mode +1)
+;;(define-key projectile-mode-map (kbd "s-p") 'projectile-command-map)
+;;(define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
 
-
+(setq c-default-style "linux" c-basic-offset 4)
